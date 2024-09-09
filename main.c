64c77286f74f44dd9350cdf399a863d90a750530
prodbysky <kyrbynyt@gmail.com>
2024-09-09 20:22:36 +0300

    Textures and basic terrain

--- main.c
+++ main.c
@@ -1,3 +1,6 @@
+#define FNL_IMPL
+#include "external/FastNoiseLite.h"
+
 #include <raylib.h>
 #include <raymath.h>
 
@@ -34,19 +37,42 @@ typedef struct {
     Rectangle player;
     Camera2D camera;
     World world;
+    fnl_state fnl;
+    Texture2D water;
+    Texture2D grass;
 } GameState;
 
 static GameState state = {0};
 
 void fill_chunk(Chunk* c) {
-    for (int i = 0; i < CHUNK_W * CHUNK_W; i++) {
+    for (int i = 0; i < CHUNK_W * CHUNK_H; i++) {
         c->blocks[i] = (Block){.solid = true};
     }
 }
 
 void random_chunk(Chunk* c) {
-    for (int i = 0; i < CHUNK_W * CHUNK_W; i++) {
+    for (int i = 0; i < CHUNK_W * CHUNK_H; i++) {
         c->blocks[i] = (Block){.solid = GetRandomValue(0, 1)};
+    }
+}
+
+void simple_perlin_chunk(Chunk* c) {
+
+    for (int x = 0; x < CHUNK_H; x++) {
+        for (int y = 0; y < CHUNK_W; y++) {
+            Vector2 block_world_pos =
+                Vector2Multiply(c->chunk_offset, (Vector2){CHUNK_W, CHUNK_H});
+            block_world_pos.x += x;
+            block_world_pos.y += y;
+            block_world_pos.x /= 100.0;
+            block_world_pos.y /= 100.0;
+            const bool s = roundf((fnlGetNoise2D(&state.fnl, block_world_pos.x,
+                                                 block_world_pos.y) +
+                                   1.8) /
+                                  2.0);
+
+            c->blocks[y * CHUNK_W + x] = (Block){s};
+        }
     }
 }
 
@@ -62,9 +88,30 @@ void draw_chunk(const Chunk* c) {
             };
 
             if (b.solid) {
-                DrawRectangle(pos_in_world.x, pos_in_world.y, TILE_SIZE,
-                              TILE_SIZE, GRAY);
+                DrawTexturePro(state.grass,
+                               (Rectangle){.x = pos_in_world.x,
+                                           .y = pos_in_world.y,
+                                           TILE_SIZE / 2.0,
+                                           TILE_SIZE / 2.0},
+
+                               (Rectangle){.x = pos_in_world.x,
+                                           .y = pos_in_world.y,
+                                           TILE_SIZE,
+                                           TILE_SIZE},
+                               Vector2Zero(), 0, WHITE);
+                continue;
             }
+            DrawTexturePro(state.water,
+                           (Rectangle){.x = pos_in_world.x,
+                                       .y = pos_in_world.y,
+                                       TILE_SIZE / 2.0,
+                                       TILE_SIZE / 2.0},
+
+                           (Rectangle){.x = pos_in_world.x,
+                                       .y = pos_in_world.y,
+                                       TILE_SIZE,
+                                       TILE_SIZE},
+                           Vector2Zero(), 0, WHITE);
         }
     }
 }
@@ -82,6 +129,7 @@ World init_world() {
                   .height = CHUNK_H * TILE_SIZE,
             };
             c->initialized = false;
+            c->visible     = false;
         }
     }
 
@@ -95,14 +143,13 @@ void update_world(World* w) {
                                    .y      = window_pos.y,
                                    .width  = WINDOW_W,
                                    .height = WINDOW_H};
-
     for (int y = 0; y < NY_CHUNKS; y++) {
         for (int x = 0; x < NX_CHUNKS; x++) {
             Chunk* c   = &w->chunks[y * NX_CHUNKS + x];
             c->visible = (CheckCollisionRecs(window_rect, c->chunk_rect));
 
             if (c->visible && !c->initialized) {
-                random_chunk(c);
+                simple_perlin_chunk(c);
                 c->initialized = true;
             }
         }
@@ -122,15 +169,20 @@ void draw_world(World* w) {
 
 void init_game() {
     InitWindow(WINDOW_W, WINDOW_H, "MicroCraft");
-    // SetTargetFPS(120);
-    state.player =
-        (Rectangle){.x = 0, .y = 0, .width = TILE_SIZE, .height = TILE_SIZE};
+    SetTargetFPS(120);
+    state.player = (Rectangle){
+        .x = 800, .y = 800, .width = TILE_SIZE, .height = TILE_SIZE};
     state.camera.zoom     = 1;
     state.camera.rotation = 0;
     state.camera.target   = (Vector2){state.player.x + state.player.width / 2,
                                       state.player.y + state.player.height / 2};
     state.camera.offset   = (Vector2){WINDOW_W / 2.0, WINDOW_H / 2.0};
     state.world           = init_world();
+    state.fnl             = fnlCreateState();
+    state.fnl.noise_type  = FNL_NOISE_OPENSIMPLEX2;
+    state.fnl.frequency   = 5;
+    state.water           = LoadTexture("water.png");
+    state.grass           = LoadTexture("grass.png");
 }
 
 void update_game() {
