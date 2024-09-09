fcc194a531c2e199013ebac0e9fce7b3e8f0ffb2
prodbysky <kyrbynyt@gmail.com>
2024-09-09 18:54:48 +0300

    Many optimizations
    - Chunks are only initialized when they first come into view
     - Chunks are window space culled

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <raylib.h>
+#include <raymath.h>
 
 #define SPEED 250
 #define WINDOW_W 1280
@@ -8,8 +9,8 @@
 
 #define CHUNK_W 8
 #define CHUNK_H 8
-#define NX_CHUNKS 8
-#define NY_CHUNKS 8
+#define NX_CHUNKS 200
+#define NY_CHUNKS 200
 
 #define TILE_SIZE 64
 
@@ -20,7 +21,22 @@ typedef struct {
 typedef struct {
     Block blocks[CHUNK_W * CHUNK_H];
     Vector2 chunk_offset;
+    Rectangle chunk_rect;
+    bool visible;
+    bool initialized;
 } Chunk;
+
+typedef struct {
+    Chunk chunks[NX_CHUNKS * NY_CHUNKS];
+} World;
+
+typedef struct {
+    Rectangle player;
+    Camera2D camera;
+    World world;
+} GameState;
+
+static GameState state = {0};
 
 void fill_chunk(Chunk* c) {
     for (int i = 0; i < CHUNK_W * CHUNK_W; i++) {
@@ -34,7 +50,7 @@ void random_chunk(Chunk* c) {
     }
 }
 
-void draw_chunk(Chunk* c) {
+void draw_chunk(const Chunk* c) {
     for (int y = 0; y < CHUNK_H; y++) {
         for (int x = 0; x < CHUNK_W; x++) {
             Block b                    = c->blocks[y * CHUNK_W + x];
@@ -53,82 +69,107 @@ void draw_chunk(Chunk* c) {
     }
 }
 
-typedef struct {
-    Chunk chunks[NX_CHUNKS * NY_CHUNKS];
-} World;
-
 World init_world() {
     World w;
     for (int y = 0; y < NY_CHUNKS; y++) {
         for (int x = 0; x < NX_CHUNKS; x++) {
             Chunk* c        = &w.chunks[y * NX_CHUNKS + x];
             c->chunk_offset = (Vector2){.x = x, .y = y};
-            random_chunk(c);
+            c->chunk_rect   = (Rectangle){
+                  .x      = c->chunk_offset.x * CHUNK_W * TILE_SIZE,
+                  .y      = c->chunk_offset.y * CHUNK_H * TILE_SIZE,
+                  .width  = CHUNK_W * TILE_SIZE,
+                  .height = CHUNK_H * TILE_SIZE,
+            };
+            c->initialized = false;
         }
     }
 
     return w;
 }
 
-void draw_world(World* w) {
+void update_world(World* w) {
+    const Vector2 window_pos = Vector2Subtract(
+        state.camera.target, (Vector2){WINDOW_W / 2.0, WINDOW_H / 2.0});
+    const Rectangle window_rect = {.x      = window_pos.x,
+                                   .y      = window_pos.y,
+                                   .width  = WINDOW_W,
+                                   .height = WINDOW_H};
+
     for (int y = 0; y < NY_CHUNKS; y++) {
         for (int x = 0; x < NX_CHUNKS; x++) {
-            draw_chunk(&w->chunks[y * NX_CHUNKS + x]);
+            Chunk* c   = &w->chunks[y * NX_CHUNKS + x];
+            c->visible = (CheckCollisionRecs(window_rect, c->chunk_rect));
+
+            if (c->visible && !c->initialized) {
+                random_chunk(c);
+                c->initialized = true;
+            }
         }
     }
 }
 
-typedef struct {
-    Rectangle player;
-    Camera2D camera;
-    World world;
-} GameState;
-
-static GameState state = {0};
+void draw_world(World* w) {
+    for (int y = 0; y < NY_CHUNKS; y++) {
+        for (int x = 0; x < NX_CHUNKS; x++) {
+            const Chunk* c = &w->chunks[y * NX_CHUNKS + x];
+            if (c->visible) {
+                draw_chunk(c);
+            }
+        }
+    }
+}
 
 void init_game() {
     InitWindow(WINDOW_W, WINDOW_H, "MicroCraft");
-    SetTargetFPS(120);
+    // SetTargetFPS(120);
     state.player =
         (Rectangle){.x = 0, .y = 0, .width = TILE_SIZE, .height = TILE_SIZE};
     state.camera.zoom     = 1;
     state.camera.rotation = 0;
     state.camera.target   = (Vector2){state.player.x + state.player.width / 2,
                                       state.player.y + state.player.height / 2};
-    state.camera.offset   = (Vector2){WINDOW_W / 2.0f, WINDOW_H / 2.0f};
+    state.camera.offset   = (Vector2){WINDOW_W / 2.0, WINDOW_H / 2.0};
     state.world           = init_world();
+}
+
+void update_game() {
+    state.camera.target = (Vector2){state.player.x + state.player.width / 2,
+                                    state.player.y + state.player.height / 2};
+
+    if (IsKeyDown(KEY_A)) {
+        state.player.x -= SPEED * GetFrameTime();
+    }
+    if (IsKeyDown(KEY_D)) {
+        state.player.x += SPEED * GetFrameTime();
+    }
+    if (IsKeyDown(KEY_W)) {
+        state.player.y -= SPEED * GetFrameTime();
+    }
+    if (IsKeyDown(KEY_S)) {
+        state.player.y += SPEED * GetFrameTime();
+    }
+    update_world(&state.world);
+}
+
+void draw_game() {
+
+    BeginDrawing();
+    ClearBackground(GetColor(0x181818ff));
+    BeginMode2D(state.camera);
+    draw_world(&state.world);
+    DrawRectangleRec(state.player, WHITE);
+    EndMode2D();
+    DrawFPS(10, 10);
+    EndDrawing();
 }
 
 int main() {
     init_game();
 
     while (!WindowShouldClose()) {
-        const Vector2 fps_pos =
-            GetScreenToWorld2D((Vector2){10, 10}, state.camera);
-        state.camera.target =
-            (Vector2){state.player.x + state.player.width / 2,
-                      state.player.y + state.player.height / 2};
-
-        if (IsKeyDown(KEY_A)) {
-            state.player.x -= SPEED * GetFrameTime();
-        }
-        if (IsKeyDown(KEY_D)) {
-            state.player.x += SPEED * GetFrameTime();
-        }
-        if (IsKeyDown(KEY_W)) {
-            state.player.y -= SPEED * GetFrameTime();
-        }
-        if (IsKeyDown(KEY_S)) {
-            state.player.y += SPEED * GetFrameTime();
-        }
-        BeginDrawing();
-        ClearBackground(GetColor(0x181818ff));
-        DrawFPS(10, 10);
-        BeginMode2D(state.camera);
-        draw_world(&state.world);
-        DrawRectangleRec(state.player, WHITE);
-        EndMode2D();
-        EndDrawing();
+        update_game();
+        draw_game();
     }
 
     CloseWindow();
